21afa78c5b163b90e2dc3404548c477ca7c4e5d1
Matias Ezequiel Manchula <ezelmanchula@gmail.com>
2024-05-16 00:10:54 -0300

    con boton subir

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         alert('Por favor complete todos los campos obligatorios.');
     }
 });
+//funciones para mi boton volver arriba
+function volverArriba() {
+    document.body.scrollTop = 0; // Para navegadores antiguos
+    document.documentElement.scrollTop = 0; // Para navegadores modernos
+}
+    
+    // Mostrar o ocultar el botón dependiendo de la posición de desplazamiento
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById('volverArriba').style.display = 'block';
+    } else {
+        document.getElementById('volverArriba').style.display = 'none';
+    }
+};
